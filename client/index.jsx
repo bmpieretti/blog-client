87dea2446e27fc9b2a4818e78f530195df35a98e
Brook Pieretti <bmpieretti@gmail.com>
2017-11-18 19:54:38 -0600

    Initialized new repo

---
+++ client/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './modules/core/components/App';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+});
---
+++ client/modules/core/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+// static propTypes = {}
+// static defaultProps = {};
+
+  render() {
+    return (
+        <h1>Hello World</h1>
+    );
+  }
+}
