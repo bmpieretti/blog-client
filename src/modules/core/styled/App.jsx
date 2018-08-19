4566627521c40dca4f991292d807ac0edf23c626
Brook Pieretti <bmpieretti@gmail.com>
2018-08-18 19:56:59 -0500

    Updated css modules to use styled components

---
+++ src/modules/core/styled/App.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+// eslint-disable-next-line import/prefer-default-export
+export const Main = styled.main`
+  height: 100%;
+`;
---
+++ src/styles/Base.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { red } from '../styles/colors';
+
+// eslint-disable-next-line import/prefer-default-export
+export const ErrorP = styled.p`
+  color: ${red};
+`;
