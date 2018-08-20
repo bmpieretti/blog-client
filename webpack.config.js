97aac61c22715e5d8bd422f6576bdb6fe8db4eb4
Brook Pieretti <bmpieretti@gmail.com>
2018-08-19 22:49:21 -0500

    Added image optimization to webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,27 @@ const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const manifest = require('./dist/vendor.json');
 
+const options = isDev => ({
+  disable: isDev,
+  mozjpeg: {
+    progressive: true,
+    quality: 65,
+  },
+  optipng: {
+    enabled: false,
+  },
+  pngquant: {
+    quality: '65-90',
+    speed: 4,
+  },
+  gifsicle: {
+    interlaced: false,
+  },
+  webp: {
+    quality: 75,
+  }
+});
+
 module.exports = (env, argv) => {
   const isDev = argv.mode === 'development';
   const moduleName = (isDev) ? '[name].[hash]' : '[name].[chunkhash].min';
@@ -50,6 +71,38 @@ module.exports = (env, argv) => {
           test: /\.(js|jsx)$/,
           include: path.resolve(__dirname, 'src'),
           loader: 'babel-loader'
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 10 * 1024,
+                publicPath: 'assets/'
+              }
+            },
+            {
+              loader: 'image-webpack-loader',
+              options: options(isDev)
+            }
+          ]
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: 'svg-url-loader',
+              options: {
+                limit: 10 * 1024,
+                noquotes: true,
+              }
+            },
+            {
+              loader: 'image-webpack-loader',
+              options: options(isDev)
+            }
+          ]
         }
       ]
     },
