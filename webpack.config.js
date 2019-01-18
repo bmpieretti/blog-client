093f7274d056ac97951cc5166f7279f5ef0717ca
Brook Pieretti <bmpieretti@gmail.com>
2019-01-17 22:13:18 -0600

    Update deps recklessly

--- webpack.config.js
+++ webpack.config.js
@@ -1,131 +1,122 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const manifest = require('./dist/vendor.json');
+const InlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
+const env = process.env.NODE_ENV;
 
 const options = isDev => ({
   disable: isDev,
   mozjpeg: {
     progressive: true,
-    quality: 65,
+    quality: 65
   },
   optipng: {
-    enabled: false,
+    enabled: false
   },
   pngquant: {
     quality: '65-90',
-    speed: 4,
+    speed: 4
   },
   gifsicle: {
-    interlaced: false,
+    interlaced: false
   },
   webp: {
-    quality: 75,
+    quality: 75
   }
 });
 
-module.exports = (env, argv) => {
-  const isDev = argv.mode === 'development';
-  const moduleName = (isDev) ? '[name].[hash]' : '[name].[chunkhash].min';
-  let entryOptions = {};
-  let plugins = [];
+const isDev = env === 'development';
+let entryOptions = {};
+let plugins = [];
 
-  if (isDev) {
-    entryOptions = {
-      hotloader: 'react-hot-loader/patch'
-    };
+if (isDev) {
+  entryOptions = {
+    hotloader: 'react-hot-loader/patch'
+  };
 
-    plugins = [
-      new webpack.HotModuleReplacementPlugin()
-    ];
-  }
+  plugins = [
+    new webpack.HotModuleReplacementPlugin()
+  ];
+}
 
-  return ({
-    entry: {
-      app: './src/index.jsx',
-      ...entryOptions
-    },
-    output: {
-      filename: `${moduleName}.js`
-    },
-    devServer: (isDev) ? {
-      hot: true,
-      contentBase: './dist',
-      port: 3000
-    } : undefined,
-    devtool: (isDev) ? 'inline-source-map' : false,
-    optimization: {
-      minimizer: [
-        new UglifyJsPlugin({
-          cache: true,
-          parallel: true,
-          sourceMap: isDev
-        })
-      ]
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          include: path.resolve(__dirname, 'src'),
-          loader: 'babel-loader'
-        },
-        {
-          test: /\.(png|jpe?g|gif)$/i,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 10 * 1024,
-                publicPath: 'assets/'
-              }
-            },
-            {
-              loader: 'image-webpack-loader',
-              options: options(isDev)
+module.exports = {
+  entry: {
+    app: './src/index.jsx',
+    ...entryOptions
+  },
+  output: {
+    filename: (isDev) ? '[name].js' : '[name].[chunkhash].min.js'
+  },
+  devServer: (isDev) ? {
+    hot: true,
+    contentBase: './dist',
+    port: 3000
+  } : undefined,
+  devtool: (isDev) ? 'source-map' : false,
+  mode: (env !== 'production') ? 'development' : 'production',
+  optimization: {
+    runtimeChunk: true,
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10 * 1024,
+              publicPath: 'assets/'
             }
-          ]
-        },
-        {
-          test: /\.svg$/,
-          use: [
-            {
-              loader: 'svg-url-loader',
-              options: {
-                limit: 10 * 1024,
-                noquotes: true,
-              }
-            },
-            {
-              loader: 'image-webpack-loader',
-              options: options(isDev)
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: options(isDev)
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10 * 1024,
+              noquotes: true
             }
-          ]
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['.js', '.jsx']
-    },
-    plugins: [
-      ...plugins,
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': `"${argv.mode}"`
-      }),
-      new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        openAnalyzer: false
-      }),
-      new webpack.DllReferencePlugin({
-        manifest
-      }),
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        inject: 'body'
-      })
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: options(isDev)
+          }
+        ]
+      }
     ]
-  });
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+      inlineSource: 'runtime~.+\\.js'
+    }),
+    new InlineSourcePlugin(),
+    new webpack.HashedModuleIdsPlugin(),
+    ...plugins
+  ]
 };
